7a8a0e5633e7c91ef2fdd4b556a6d9e97e46f254
Aditya Tayade <71881484+OberonPrime117@users.noreply.github.com>
2023-02-10 18:40:55 +0530

    doc , index

--- elastic.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-from elasticsearch import Elasticsearch
-ELASTIC_PASSWORD = "-jnEVq4pt66m9GDJpxQy"
-es = Elasticsearch("http://localhost:9200",basic_auth=("elastic", ELASTIC_PASSWORD))
-print(es.info().body)
